f79ddb6df5cccceaa5947cae6de121244e4fd305
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-03-08 15:51:55 -0300

    classe MarcaProduto criada

---
+++ src/main/java/com/arthenyo/Loja/model/MarcaProduto.java
@@ -0,0 +1,52 @@
+package com.arthenyo.Loja.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto",allocationSize = 1, initialValue = 1)
+public class MarcaProduto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+    private String nomeDesc;
+
+    public MarcaProduto() {
+    }
+
+    public MarcaProduto(Long id, String nomeDesc) {
+        this.id = id;
+        this.nomeDesc = nomeDesc;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MarcaProduto that = (MarcaProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
