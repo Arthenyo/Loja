8e109ce9fc1d193d0817067827791411e549e2a7
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-03-08 16:09:21 -0300

    classe acesso criada

---
+++ src/main/java/com/arthenyo/Loja/model/Acesso.java
@@ -0,0 +1,37 @@
+package com.arthenyo.Loja.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_acesso")
+@SequenceGenerator(name = "seq_acesso",sequenceName = "seq_acesso",allocationSize = 1, initialValue = 1)
+public class Acesso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+    @Column(nullable = false)
+    private String descricao;
+
+    public Acesso() {
+    }
+    public Acesso(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
