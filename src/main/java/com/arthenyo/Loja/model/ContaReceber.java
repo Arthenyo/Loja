b900ab84e9e5c38b8852399b76e506340f38ac03
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-03-10 14:15:26 -0300

    manutenção do banco e classe contaReceber criada

---
+++ src/main/java/com/arthenyo/Loja/model/ContaReceber.java
@@ -0,0 +1,118 @@
+package com.arthenyo.Loja.model;
+
+import com.arthenyo.Loja.enums.StatusContaReceber;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_conta_receber")
+@SequenceGenerator(name = "seq_conta_receber", sequenceName = "seq_conta_receber", allocationSize = 1, initialValue = 1)
+public class ContaReceber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
+    private Long id;
+    private String descricao;
+    @Enumerated(EnumType.STRING)
+    private StatusContaReceber status;
+    private LocalDate dtVencimento;
+    private LocalDate dtPagemnto;
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id",nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    public ContaReceber() {
+    }
+
+    public ContaReceber(Long id, String descricao, StatusContaReceber status, LocalDate dtVencimento, LocalDate dtPagemnto, BigDecimal valorTotal, BigDecimal valorDesconto, Pessoa pessoa) {
+        this.id = id;
+        this.descricao = descricao;
+        this.status = status;
+        this.dtVencimento = dtVencimento;
+        this.dtPagemnto = dtPagemnto;
+        this.valorTotal = valorTotal;
+        this.valorDesconto = valorDesconto;
+        this.pessoa = pessoa;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusContaReceber getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusContaReceber status) {
+        this.status = status;
+    }
+
+    public LocalDate getDtVencimento() {
+        return dtVencimento;
+    }
+
+    public void setDtVencimento(LocalDate dtVencimento) {
+        this.dtVencimento = dtVencimento;
+    }
+
+    public LocalDate getDtPagemnto() {
+        return dtPagemnto;
+    }
+
+    public void setDtPagemnto(LocalDate dtPagemnto) {
+        this.dtPagemnto = dtPagemnto;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContaReceber that = (ContaReceber) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
